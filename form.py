2731bd00868432dab3e6c842e8a101a2fd826339
maximainero <32918230+maximainero@users.noreply.github.com>
2020-03-11 01:15:51 -0300

    agregando reloj binario en qt-py

---
+++ form.py
@@ -0,0 +1,463 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'form.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+h1 = 0
+h2 = 0
+m1 = 0
+m2 = 0
+s1 = 0
+s2 = 0
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(600, 329)
+        self.pushButton_hora_a_8 = QtWidgets.QPushButton(Form)
+        self.pushButton_hora_a_8.setGeometry(QtCore.QRect(20, 30, 61, 51))
+        self.pushButton_hora_a_8.setText("")
+        self.pushButton_hora_a_8.setCheckable(True)
+        self.pushButton_hora_a_8.setChecked(False)
+        self.pushButton_hora_a_8.setObjectName("pushButton_hora_a_8")
+        self.pushButton_hora_a_8.setEnabled(False)
+        self.pushButton_hora_b_8 = QtWidgets.QPushButton(Form)
+        self.pushButton_hora_b_8.setGeometry(QtCore.QRect(90, 30, 61, 51))
+        self.pushButton_hora_b_8.setText("")
+        self.pushButton_hora_b_8.setCheckable(True)
+        self.pushButton_hora_b_8.setChecked(False)
+        self.pushButton_hora_b_8.setObjectName("pushButton_hora_b_8")
+        self.pushButton_segundos_b_8 = QtWidgets.QPushButton(Form)
+        self.pushButton_segundos_b_8.setGeometry(QtCore.QRect(430, 30, 61, 51))
+        self.pushButton_segundos_b_8.setText("")
+        self.pushButton_segundos_b_8.setCheckable(True)
+        self.pushButton_segundos_b_8.setChecked(False)
+        self.pushButton_segundos_b_8.setObjectName("pushButton_segundos_b_8")
+        self.pushButton_segundos_a_8 = QtWidgets.QPushButton(Form)
+        self.pushButton_segundos_a_8.setGeometry(QtCore.QRect(360, 30, 61, 51))
+        self.pushButton_segundos_a_8.setText("")
+        self.pushButton_segundos_a_8.setCheckable(True)
+        self.pushButton_segundos_a_8.setChecked(False)
+        self.pushButton_segundos_a_8.setObjectName("pushButton_segundos_a_8")
+        self.pushButton_segundos_a_8.setEnabled(False)
+        self.pushButton_minutos_b_8 = QtWidgets.QPushButton(Form)
+        self.pushButton_minutos_b_8.setGeometry(QtCore.QRect(260, 30, 61, 51))
+        self.pushButton_minutos_b_8.setText("")
+        self.pushButton_minutos_b_8.setCheckable(True)
+        self.pushButton_minutos_b_8.setChecked(False)
+        self.pushButton_minutos_b_8.setObjectName("pushButton_minutos_b_8")
+        self.pushButton_minutos_a_8 = QtWidgets.QPushButton(Form)
+        self.pushButton_minutos_a_8.setGeometry(QtCore.QRect(190, 30, 61, 51))
+        self.pushButton_minutos_a_8.setText("")
+        self.pushButton_minutos_a_8.setCheckable(True)
+        self.pushButton_minutos_a_8.setChecked(False)
+        self.pushButton_minutos_a_8.setObjectName("pushButton_minutos_a_8")
+        self.pushButton_minutos_a_8.setEnabled(False)
+        self.pushButton_segundos_b_4 = QtWidgets.QPushButton(Form)
+        self.pushButton_segundos_b_4.setGeometry(QtCore.QRect(430, 90, 61, 51))
+        self.pushButton_segundos_b_4.setText("")
+        self.pushButton_segundos_b_4.setCheckable(True)
+        self.pushButton_segundos_b_4.setChecked(False)
+        self.pushButton_segundos_b_4.setObjectName("pushButton_segundos_b_4")
+        self.pushButton_hora_b_4 = QtWidgets.QPushButton(Form)
+        self.pushButton_hora_b_4.setGeometry(QtCore.QRect(90, 90, 61, 51))
+        self.pushButton_hora_b_4.setText("")
+        self.pushButton_hora_b_4.setCheckable(True)
+        self.pushButton_hora_b_4.setChecked(False)
+        self.pushButton_hora_b_4.setObjectName("pushButton_hora_b_4")
+        self.pushButton_hora_a_4 = QtWidgets.QPushButton(Form)
+        self.pushButton_hora_a_4.setGeometry(QtCore.QRect(20, 90, 61, 51))
+        self.pushButton_hora_a_4.setText("")
+        self.pushButton_hora_a_4.setCheckable(True)
+        self.pushButton_hora_a_4.setChecked(False)
+        self.pushButton_hora_a_4.setObjectName("pushButton_hora_a_4")
+        self.pushButton_hora_a_4.setEnabled(False)
+        self.pushButton_segundos_a_4 = QtWidgets.QPushButton(Form)
+        self.pushButton_segundos_a_4.setGeometry(QtCore.QRect(360, 90, 61, 51))
+        self.pushButton_segundos_a_4.setText("")
+        self.pushButton_segundos_a_4.setCheckable(True)
+        self.pushButton_segundos_a_4.setChecked(False)
+        self.pushButton_segundos_a_4.setObjectName("pushButton_segundos_a_4")
+        self.pushButton_segundos_a_4.setEnabled(False)
+        self.pushButton_minutos_b_4 = QtWidgets.QPushButton(Form)
+        self.pushButton_minutos_b_4.setGeometry(QtCore.QRect(260, 90, 61, 51))
+        self.pushButton_minutos_b_4.setText("")
+        self.pushButton_minutos_b_4.setCheckable(True)
+        self.pushButton_minutos_b_4.setChecked(False)
+        self.pushButton_minutos_b_4.setObjectName("pushButton_minutos_b_4")
+        self.pushButton_minutos_a_4 = QtWidgets.QPushButton(Form)
+        self.pushButton_minutos_a_4.setGeometry(QtCore.QRect(190, 90, 61, 51))
+        self.pushButton_minutos_a_4.setText("")
+        self.pushButton_minutos_a_4.setCheckable(True)
+        self.pushButton_minutos_a_4.setChecked(False)
+        self.pushButton_minutos_a_4.setObjectName("pushButton_minutos_a_4")
+        self.pushButton_minutos_a_4.setEnabled(False)
+        self.pushButton_segundos_b_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_segundos_b_2.setGeometry(QtCore.QRect(430, 150, 61, 51))
+        self.pushButton_segundos_b_2.setText("")
+        self.pushButton_segundos_b_2.setCheckable(True)
+        self.pushButton_segundos_b_2.setChecked(False)
+        self.pushButton_segundos_b_2.setObjectName("pushButton_segundos_b_2")
+        self.pushButton_segundos_a_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_segundos_a_2.setGeometry(QtCore.QRect(360, 150, 61, 51))
+        self.pushButton_segundos_a_2.setText("")
+        self.pushButton_segundos_a_2.setCheckable(True)
+        self.pushButton_segundos_a_2.setChecked(False)
+        self.pushButton_segundos_a_2.setObjectName("pushButton_segundos_a_2")
+        self.pushButton_hora_b_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_hora_b_2.setGeometry(QtCore.QRect(90, 150, 61, 51))
+        self.pushButton_hora_b_2.setText("")
+        self.pushButton_hora_b_2.setCheckable(True)
+        self.pushButton_hora_b_2.setChecked(False)
+        self.pushButton_hora_b_2.setObjectName("pushButton_hora_b_2")
+        self.pushButton_minutos_a_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_minutos_a_2.setGeometry(QtCore.QRect(190, 150, 61, 51))
+        self.pushButton_minutos_a_2.setText("")
+        self.pushButton_minutos_a_2.setCheckable(True)
+        self.pushButton_minutos_a_2.setChecked(False)
+        self.pushButton_minutos_a_2.setObjectName("pushButton_minutos_a_2")
+        self.pushButton_hora_a_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_hora_a_2.setGeometry(QtCore.QRect(20, 150, 61, 51))
+        self.pushButton_hora_a_2.setText("")
+        self.pushButton_hora_a_2.setCheckable(True)
+        self.pushButton_hora_a_2.setChecked(False)
+        self.pushButton_hora_a_2.setObjectName("pushButton_hora_a_2")
+        self.pushButton_minutos_b_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_minutos_b_2.setGeometry(QtCore.QRect(260, 150, 61, 51))
+        self.pushButton_minutos_b_2.setText("")
+        self.pushButton_minutos_b_2.setCheckable(True)
+        self.pushButton_minutos_b_2.setChecked(False)
+        self.pushButton_minutos_b_2.setObjectName("pushButton_minutos_b_2")
+        self.pushButton_segundos_b_1 = QtWidgets.QPushButton(Form)
+        self.pushButton_segundos_b_1.setGeometry(QtCore.QRect(430, 210, 61, 51))
+        self.pushButton_segundos_b_1.setText("")
+        self.pushButton_segundos_b_1.setCheckable(True)
+        self.pushButton_segundos_b_1.setChecked(False)
+        self.pushButton_segundos_b_1.setObjectName("pushButton_segundos_b_1")
+        self.pushButton_segundos_a_1 = QtWidgets.QPushButton(Form)
+        self.pushButton_segundos_a_1.setGeometry(QtCore.QRect(360, 210, 61, 51))
+        self.pushButton_segundos_a_1.setText("")
+        self.pushButton_segundos_a_1.setCheckable(True)
+        self.pushButton_segundos_a_1.setChecked(False)
+        self.pushButton_segundos_a_1.setObjectName("pushButton_segundos_a_1")
+        self.pushButton_hora_b_1 = QtWidgets.QPushButton(Form)
+        self.pushButton_hora_b_1.setGeometry(QtCore.QRect(90, 210, 61, 51))
+        self.pushButton_hora_b_1.setText("")
+        self.pushButton_hora_b_1.setCheckable(True)
+        self.pushButton_hora_b_1.setChecked(False)
+        self.pushButton_hora_b_1.setObjectName("pushButton_hora_b_1")
+        self.pushButton_minutos_a_1 = QtWidgets.QPushButton(Form)
+        self.pushButton_minutos_a_1.setGeometry(QtCore.QRect(190, 210, 61, 51))
+        self.pushButton_minutos_a_1.setText("")
+        self.pushButton_minutos_a_1.setCheckable(True)
+        self.pushButton_minutos_a_1.setChecked(False)
+        self.pushButton_minutos_a_1.setObjectName("pushButton_minutos_a_1")
+        self.pushButton_hora_a_1 = QtWidgets.QPushButton(Form)
+        self.pushButton_hora_a_1.setGeometry(QtCore.QRect(20, 210, 61, 51))
+        self.pushButton_hora_a_1.setText("")
+        self.pushButton_hora_a_1.setCheckable(True)
+        self.pushButton_hora_a_1.setChecked(False)
+        self.pushButton_hora_a_1.setObjectName("pushButton_hora_a_1")
+        self.pushButton_minutos_b_1 = QtWidgets.QPushButton(Form)
+        self.pushButton_minutos_b_1.setGeometry(QtCore.QRect(260, 210, 61, 51))
+        self.pushButton_minutos_b_1.setText("")
+        self.pushButton_minutos_b_1.setCheckable(True)
+        self.pushButton_minutos_b_1.setChecked(False)
+        self.pushButton_minutos_b_1.setObjectName("pushButton_minutos_b_1")
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(550, 30, 57, 51))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Form)
+        self.label_2.setGeometry(QtCore.QRect(550, 90, 57, 51))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(Form)
+        self.label_3.setGeometry(QtCore.QRect(550, 150, 57, 51))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(Form)
+        self.label_4.setGeometry(QtCore.QRect(550, 210, 57, 51))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
+        self.h1 = QtWidgets.QLabel(Form)
+        self.h1.setGeometry(QtCore.QRect(40, 270, 57, 51))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.h1.setFont(font)
+        self.h1.setObjectName("h1")
+        self.h2 = QtWidgets.QLabel(Form)
+        self.h2.setGeometry(QtCore.QRect(110, 270, 57, 51))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.h2.setFont(font)
+        self.h2.setObjectName("h2")
+        self.label_7 = QtWidgets.QLabel(Form)
+        self.label_7.setGeometry(QtCore.QRect(160, 270, 57, 51))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.label_7.setFont(font)
+        self.label_7.setObjectName("label_7")
+        self.m2 = QtWidgets.QLabel(Form)
+        self.m2.setGeometry(QtCore.QRect(280, 270, 57, 51))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.m2.setFont(font)
+        self.m2.setObjectName("m2")
+        self.label_9 = QtWidgets.QLabel(Form)
+        self.label_9.setGeometry(QtCore.QRect(330, 270, 57, 51))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.label_9.setFont(font)
+        self.label_9.setObjectName("label_9")
+        self.m1 = QtWidgets.QLabel(Form)
+        self.m1.setGeometry(QtCore.QRect(210, 270, 57, 51))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.m1.setFont(font)
+        self.m1.setObjectName("m1")
+        self.s2 = QtWidgets.QLabel(Form)
+        self.s2.setGeometry(QtCore.QRect(450, 270, 57, 51))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.s2.setFont(font)
+        self.s2.setObjectName("s2")
+        self.s1 = QtWidgets.QLabel(Form)
+        self.s1.setGeometry(QtCore.QRect(380, 270, 57, 51))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.s1.setFont(font)
+        self.s1.setObjectName("s1")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+        self.pushButton_hora_a_1.clicked.connect(self.agregar_ha1)
+        self.pushButton_hora_a_2.clicked.connect(self.agregar_ha2)
+
+        self.pushButton_hora_b_2.clicked.connect(self.agregar_hb2)
+        self.pushButton_hora_b_1.clicked.connect(self.agregar_hb1)
+        self.pushButton_hora_b_4.clicked.connect(self.agregar_hb4)
+        self.pushButton_hora_b_8.clicked.connect(self.agregar_hb8)
+
+
+        self.pushButton_minutos_a_1.clicked.connect(self.agregar_ma1)
+        self.pushButton_minutos_a_2.clicked.connect(self.agregar_ma2)
+
+        self.pushButton_minutos_b_2.clicked.connect(self.agregar_mb2)
+        self.pushButton_minutos_b_1.clicked.connect(self.agregar_mb1)
+        self.pushButton_minutos_b_4.clicked.connect(self.agregar_mb4)
+        self.pushButton_minutos_b_8.clicked.connect(self.agregar_mb8)
+
+
+        self.pushButton_segundos_a_1.clicked.connect(self.agregar_sa1)
+        self.pushButton_segundos_a_2.clicked.connect(self.agregar_sa2)
+
+        self.pushButton_segundos_b_2.clicked.connect(self.agregar_sb2)
+        self.pushButton_segundos_b_1.clicked.connect(self.agregar_sb1)
+        self.pushButton_segundos_b_4.clicked.connect(self.agregar_sb4)
+        self.pushButton_segundos_b_8.clicked.connect(self.agregar_sb8)
+
+
+    def agregar_ha1(self):
+        global h1
+        if self.pushButton_hora_a_1.isChecked():
+            h1 += 1
+        else:
+            h1 -= 1
+        self.h1.setText(str(h1))
+
+
+    def agregar_ha2(self):
+        global h1
+        if self.pushButton_hora_a_2.isChecked():
+            h1 += 2
+        else:
+            h1 -= 2
+        self.h1.setText(str(h1))
+
+
+    def agregar_hb1(self):
+        global h2
+        if self.pushButton_hora_b_1.isChecked():
+            h2 += 1
+        else:
+            h2 -= 1
+        self.h2.setText(str(h2))
+
+
+    def agregar_hb2(self):
+        global h2
+        if self.pushButton_hora_b_2.isChecked():
+            h2 += 2
+        else:
+            h2 -= 2
+        self.h2.setText(str(h2))
+
+
+    def agregar_hb4(self):
+        global h2
+        if self.pushButton_hora_b_4.isChecked():
+            h2 += 4
+        else:
+            h2 -= 4
+        self.h2.setText(str(h2))
+
+
+    def agregar_hb8(self):
+        global h2
+        if self.pushButton_hora_b_8.isChecked():
+            h2 += 8
+        else:
+            h2 -= 8
+        self.h2.setText(str(h2))
+
+
+    def agregar_ma1(self):
+        global m1
+        if self.pushButton_minutos_a_1.isChecked():
+            m1 += 1
+        else:
+            m1 -= 1
+        self.m1.setText(str(m1))
+
+
+    def agregar_ma2(self):
+        global m1
+        if self.pushButton_minutos_a_2.isChecked():
+            m1 += 2
+        else:
+            m1 -= 2
+        self.m1.setText(str(m1))
+
+
+    def agregar_mb1(self):
+        global m2
+        if self.pushButton_minutos_b_1.isChecked():
+            m2 += 1
+        else:
+            m2 -= 1
+        self.m2.setText(str(m2))
+
+
+    def agregar_mb2(self):
+        global m2
+        if self.pushButton_minutos_b_2.isChecked():
+            m2 += 2
+        else:
+            m2 -= 2
+        self.m2.setText(str(m2))
+
+
+    def agregar_mb4(self):
+        global m2
+        if self.pushButton_minutos_b_4.isChecked():
+            m2 += 4
+        else:
+            m2 -= 4
+        self.m2.setText(str(m2))
+
+
+    def agregar_mb8(self):
+        global m2
+        if self.pushButton_minutos_b_8.isChecked():
+            m2 += 8
+        else:
+            m2 -= 8
+        self.m2.setText(str(m2))
+
+
+    def agregar_sa1(self):
+        global s1
+        if self.pushButton_segundos_a_1.isChecked():
+            s1 += 1
+        else:
+            s1 -= 1
+        self.s1.setText(str(s1))
+
+
+    def agregar_sa2(self):
+        global s1
+        if self.pushButton_segundos_a_2.isChecked():
+            s1 += 2
+        else:
+            s1 -= 2
+        self.s1.setText(str(s1))
+
+
+    def agregar_sb1(self):
+        global s2
+        if self.pushButton_segundos_b_1.isChecked():
+            s2 += 1
+        else:
+            s2 -= 1
+        self.s2.setText(str(s2))
+
+
+    def agregar_sb2(self):
+        global s2
+        if self.pushButton_segundos_b_2.isChecked():
+            s2 += 2
+        else:
+            s2 -= 2
+        self.s2.setText(str(s2))
+
+
+    def agregar_sb4(self):
+        global s2
+        if self.pushButton_segundos_b_4.isChecked():
+            s2 += 4
+        else:
+            s2 -= 4
+        self.s2.setText(str(s2))
+
+
+    def agregar_sb8(self):
+        global s2
+        if self.pushButton_segundos_b_8.isChecked():
+            s2 += 8
+        else:
+            s2 -= 8
+        self.s2.setText(str(s2))
+
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.label.setText(_translate("Form", "8"))
+        self.label_2.setText(_translate("Form", "4"))
+        self.label_3.setText(_translate("Form", "2"))
+        self.label_4.setText(_translate("Form", "1"))
+        self.h1.setText(_translate("Form", "0"))
+        self.h2.setText(_translate("Form", "0"))
+        self.label_7.setText(_translate("Form", ":"))
+        self.m2.setText(_translate("Form", "0"))
+        self.label_9.setText(_translate("Form", ":"))
+        self.m1.setText(_translate("Form", "0"))
+        self.s2.setText(_translate("Form", "0"))
+        self.s1.setText(_translate("Form", "0"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
